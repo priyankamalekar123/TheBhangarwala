41ea8b48cfb9d79f6704056838352fb6666227a1
Priyanka <priyankamalekar1109gmail.com>
2022-07-29 10:21:43 +0530

    first commit

---
+++ app/src/main/java/com/sweeka/thebhangarwala/VendorLoginActivity.kt
@@ -0,0 +1,11 @@
+package com.sweeka.thebhangarwala
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VendorLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vendor_login)
+    }
+}
